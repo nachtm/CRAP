94ce5abcee6e8766dd739e6b7dbbe4d6bd2648f2
nachtm <nachtm@carleton.edu>
2018-04-13 15:29:45 -0500

    removed references to localhost and replaced with crap-db.herokuapp.com

--- src/actions/index.js
+++ src/actions/index.js
@@ -32,8 +32,7 @@ export function fetchCourses() {
   return dispatch => {
     dispatch(requestCourses());
     console.log(fetch);
-    // return fetch('http://crap-db.herokuapp.com/courses')
-    return fetch('http://localhost:5000/courses')
+    return fetch('http://crap-db.herokuapp.com/courses')
       .then(response => response.json())
       .then(json => dispatch(receiveCourses(json)), console.log("Something went wrong with the course request."));
   }
@@ -43,8 +42,7 @@ export function fetchCoursesBy(queryDict) {
   return dispatch => {
     dispatch(requestCourses());
     console.log(fetch)
-    let queryString = 'http://localhost:5000/courses?where=' + getEveQueryString(queryDict);
-    // let queryString = 'https://crap-db.herokuapp.com/courses?where=' + getEveQueryString(queryDict);
+    let queryString = 'https://crap-db.herokuapp.com/courses?where=' + getEveQueryString(queryDict);
     return fetch(queryString)
       .then(response => response.json())
       .then(json => dispatch(receiveCourses(json)), console.log("Something went wrong."));
@@ -70,7 +68,7 @@ export function fetchPeriods() {
   return dispatch => {
     dispatch(requestPeriods());
     console.log(fetch);
-    return fetch('http://localhost:5000/periods')
+    return fetch('http://crap-db.herokuapp.com/periods')
       .then(response => response.json())
       .then(json => dispatch(receivePeriods(json)), console.log("Something went wrong with the period request."));
   }
